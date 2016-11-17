de42d0522a53eb08da8c99ee7f66b961890b036f
uladbohdan <vlbohdan@gmail.com>
2016-11-17 20:45:28 +0300

    FreeImage dependency removed
    compiles fine without FreeImage (unused dependency removed)

--- mac_os_install.sh
+++ mac_os_install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install cmake
-brew install freeimage
 brew install ceres-solver
 brew install boost
 brew install glew
