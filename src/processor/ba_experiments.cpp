7fe2b94482cc086660ac926e42cad3fc0c963e9f
uladbohdan <vlbohdan@gmail.com>
2016-11-24 17:56:10 +0300

    BAExperiments: defining tasks, database fetcher, simple implementation

--- src/processor/ba_experiments.cpp
+++ src/processor/ba_experiments.cpp
@@ -1,7 +1,127 @@
 #include "ba_experiments.h"
 
-BAExperiments::BAExperiments(const OptionManager& options) : options_(options) {}
+// From CERES BA Examples.
+// Allows to count residual for pinhole camera. Not very useful for us.
+struct SnavelyReprojectionError {
+    SnavelyReprojectionError(double observed_x, double observed_y)
+            : observed_x(observed_x), observed_y(observed_y) {}
+
+    template<typename T>
+    bool operator()(const T* const camera,
+                    const T* const point,
+                    T* residuals) const {
+        // camera[0,1,2] are the angle-axis rotation.
+        T p[3];
+        ceres::AngleAxisRotatePoint(camera, point, p);
+        // camera[3,4,5] are the translation.
+        p[0] += camera[3];
+        p[1] += camera[4];
+        p[2] += camera[5];
+        // Compute the center of distortion. The sign change comes from
+        // the camera model that Noah Snavely's Bundler assumes, whereby
+        // the camera coordinate system has a negative z axis.
+        T xp = -p[0] / p[2];
+        T yp = -p[1] / p[2];
+        // Apply second and fourth order radial distortion.
+        const T& l1 = camera[7];
+        const T& l2 = camera[8];
+        T r2 = xp * xp + yp * yp;
+        T distortion = T(1.0) + r2 * (l1 + l2 * r2);
+        // Compute final projected point position.
+        const T& focal = camera[6];
+        T predicted_x = focal * distortion * xp;
+        T predicted_y = focal * distortion * yp;
+        // The error is the difference between the predicted and observed position.
+        residuals[0] = predicted_x - T(observed_x);
+        residuals[1] = predicted_y - T(observed_y);
+        return true;
+    }
+
+    // Factory to hide the construction of the CostFunction object from
+    // the client code.
+    static ceres::CostFunction* Create(const double observed_x,
+                                       const double observed_y) {
+        return (new ceres::AutoDiffCostFunction<SnavelyReprojectionError, 2, 9, 3>(
+                new SnavelyReprojectionError(observed_x, observed_y)));
+    }
+
+    double observed_x;
+    double observed_y;
+};
+
+BAExperiments::BAExperiments(const OptionManager& options) : options_(options), database_cache_(new DatabaseCache()) {}
 
 void BAExperiments::RunSimpleCeresImplementation() {
 
+    ceres::Problem problem;
+    BuildCeresProblem(problem);
+
+    ceres::Solver::Options options;
+    options.linear_solver_type = ceres::DENSE_SCHUR;
+    options.minimizer_progress_to_stdout = true;
+
+    ceres::Solver::Summary summary;
+    ceres::Solve(options, &problem, &summary);
+    std::cout << summary.FullReport() << "\n";
+}
+
+void BAExperiments::LoadDatabaseCache() {
+    std::cout << "Loading database cache... ";
+    Database database;
+    database.Open(*options_.database_path);
+    const size_t min_num_matches = static_cast<size_t>(options_.mapper_options->min_num_matches);
+    database_cache_->Load(database, min_num_matches, options_.mapper_options->ignore_watermarks);
+    std::cout << "finished successfully." << std::endl;
+}
+
+void BAExperiments::BuildCeresProblem(ceres::Problem& problem) {
+    std::cout << "Building CERES problem..." << std::endl;
+    LoadDatabaseCache();
+
+    std::cout << "Retrieving data from database..." << std::endl;
+    int num_images = (int) database_cache_->NumImages();
+    std::cout << "Num images: " << num_images << std::endl;
+    int num_cameras = (int) database_cache_->NumCameras();
+    std::cout << "Num cameras: " << num_cameras << std::endl;
+
+    auto images = database_cache_->Images();
+    std::cout << "Real number of images retrieved: " << images.size() << std::endl;
+    for (auto image_pair : images) {
+        Image image = image_pair.second;
+        std::cout << "Processing with image " << image.ImageId() << " :" << std::endl;
+        std::cout << "   Image has camera: " << image.HasCamera() << std::endl;
+        std::cout << "   Num points 3d: " << image.NumPoints3D() << std::endl;
+        std::cout << "   Number of observations: " << image.NumPoints2D() << " "
+                  << image.Points2D().size() << std::endl;
+
+        Camera camera = database_cache_->Camera(image.CameraId());
+        std::cout << "   Camera: " << camera.CameraId() << std::endl;
+        if (camera.ModelName() != "RADIAL") {
+            std::cout << "!! NOT RADIAL" << std::endl;
+            continue;
+        }
+        // Processing with every image and every camera.
+        auto observations = image.Points2D();
+        for (auto obs : observations) {
+            ceres::CostFunction* cost_function = SnavelyReprojectionError::Create(obs.X(), obs.Y());
+            // actually this function is for pinhole camera.
+            // where (quoting CERES):
+            // Templated pinhole camera model for used with Ceres.  The camera is
+            // parameterized using 9 parameters: 3 for rotation, 3 for translation, 1 for
+            // focal length and 2 for radial distortion. The principal point is not modeled
+            // (i.e. it is assumed be located at the image center).
+            double camera_params[9] = {1., 1., 1., 1., 1., 1., 1., 1., 1.};
+            // the camera is radial.
+            // camera.Params() are "f, cx, cy, k1, k2".
+            camera_params[6] = camera.Params()[0];
+
+
+            double point3d_approx[3] = {0., 0., 0.};
+            problem.AddResidualBlock(cost_function,
+                                     NULL,
+                                     camera_params,
+                                     point3d_approx);
+        }
+    }
+    std::cout << std::endl << "PROBLEM IS BUILT" << std::endl;
 }
--- src/processor/ba_experiments.h
+++ src/processor/ba_experiments.h
@@ -1,12 +1,26 @@
 //
 // Class to run experiments on Bundle Adjustment problem.
 //
+// Primary statements:
+// 1. To use the data from our database.
+// 2. Assuming all photos were made by the same camera, so all inner parameters are the same.
+//     Rotation & translation are not.
+// 3. Ways of estimating 3d points: * the same approach with IncrementalMapper.
+//                                  * using GPS data either retrieved from the photo itself or from other file.
+// 4. Ways of estimating camera params: * the same approach with IncrementalMapper.
+//                                      * using magic file which knows rotation etc. (from drone detectors).
+// 5. To check if it's possible to make CERES use GPU and if that makes any sense.
 
 #ifndef INC_3D_RECONSTRUCTION_BA_EXPERIMENTAL_H
 #define INC_3D_RECONSTRUCTION_BA_EXPERIMENTAL_H
 
+#include <iostream>
+
 #include "../storage.h"
 #include "../options.h"
+
+#include <ceres/ceres.h>
+#include <ceres/rotation.h>
 
 class BAExperiments {
 public:
@@ -15,7 +29,11 @@ public:
     void RunSimpleCeresImplementation();
 
 private:
+    void LoadDatabaseCache();
+    void BuildCeresProblem(ceres::Problem&);
+
     const OptionManager options_;
+    DatabaseCache* database_cache_;
 };
 
 #endif //INC_3D_RECONSTRUCTION_BA_EXPERIMENTAL_H
